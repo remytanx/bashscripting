5baa0d956168545a21f758ba1a35b2251ac6e867
remytanx <remy.tan@gmail.com>
2021-03-10 14:51:34 +0800

    Update

--- remediationRHEL8-3.sh
+++ remediationRHEL8-3.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 #AUTHOR REMY TAN#
-
+echo "Hello World!"
 display_menu(){
 	while true;
 	do
-	echo "======================================="
-	echo "Best effort Remediation for RHEL 8.3"
-	echo "======================================="
-	echo "WARNING: ONLY RUN THIS SCRIPT AFTER PERFORMED THE hardeningRhel7.sh"
-	echo "LEGEND: # - MANUAL REMEDIATION ## - FAILED"
-	echo "DO YOU WANT TO CONTINUE??"
-	echo "(Y) Yes"
-	echo "(N) No"
-	read -p "Input Selection: " selection
-	case $seleciton in
-		Y )
-			echo "Yes"
-		;;
-		N )
-			echo "No"
-		[Qq]* )
-			exit ;;
-	
+		echo "======================================="
+		echo "Best effort Remediation for RHEL 8.3"
+		echo "======================================="
+		echo "WARNING: ONLY RUN THIS SCRIPT AFTER PERFORMED THE hardeningRhel7.sh"
+		echo "LEGEND: # - MANUAL REMEDIATION ## - FAILED"
+		echo "DO YOU WANT TO CONTINUE??"
+		echo "(Y) Yes"
+		echo "(N) No"
+		echo "(q) Quit"
+		read -p "Input Selection: " selection
+		case $selection in
+			[Yy]* )
+				echo "Yes"
+			;;
+			[Nn]* )
+				echo "No"
+			;;
+			[Qq]* )
+				exit ;;
+		esac
+	done
+}
+
+## Main Function Calls
+display_menu
